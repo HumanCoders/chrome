758427203dfc88a4a6ff777654ddf2ebf0adf4f3
Camille Roux <roux.camille@gmail.com>
2012-09-04 12:11:00 +0200

    fix url

--- human-coder-news.js
+++ human-coder-news.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({
-    url: 'http://news.humancoders.com/items/new?url='+encodeURIComponent(tab.url)+"?utm_source=HCNChromeExtention&utm_medium=button&utm_campaign=share-news"
+    url: 'http://news.humancoders.com/items/new?url='+encodeURIComponent(tab.url)+"&utm_source=HCNChromeExtention&utm_medium=button&utm_campaign=share-news"
   });
 });
